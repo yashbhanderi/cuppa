90cac2253a9b543e56f13815aea624883f247a01
Yash Bhanderi <yashbhanderi1818@gmail.com>
2022-04-08 04:31:22 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import CuppaVideo from "./video/cuppa.mp4";
+import Logo from "./Logo.png";
+import "./style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import startFirebase from "./firebase";
+import { Timestamp } from "firebase/firestore";
+
+// firestore
+// import { db } from "./firebase";
+// import { collection, addDoc, Timestamp } from "firebase/firestore";
+
+// real-time
+import { ref, set } from "firebase/database";
+
+const App = () => {
+    // const slogan = ["dublin is ", "xyz is ", "abc is "];
+
+    const [feedback, setFeedback] = useState("");
+    const [email, setEmail] = useState("");
+    const [toggle, setToggle] = useState(false);
+    const [db, setDb] = useState(null);
+
+    const feedbackBtnHandler = () => {
+        if (feedback !== "") {
+            console.log("okay");
+            setToggle(true);
+        }
+    };
+
+    const emailBtnHandler = async () => {
+        // firestore
+        // try {
+        //     const docRef = await addDoc(collection(db, "feedback"), {
+        //         message: feedback,
+        //         email: email,
+        //         created: Timestamp.now(),
+        //     });
+        //     // onClose()
+        //     console.log("Document written with ID: ", docRef.id);
+        // } catch (err) {
+        //     console.log(err);
+        // }
+
+        // real-time
+        
+        const datetime = Date().toLocaleString();
+
+        set(ref(db, "feedback/"+datetime), {
+            message: feedback,
+            email: email,
+            created: new Date().toLocaleString(),
+        })
+            .then(() => {
+                console.log("data added successfully.");
+                setToggle(false);
+                setFeedback("");
+                setEmail("");
+            })
+            .catch((err) => {
+                console.log("[Error] ", err);
+            });
+    };
+
+    useEffect(() => {
+        setDb(startFirebase());
+    }, []);
+
+    return (
+        <div id="home">
+            <video id="video" autoPlay loop muted>
+                <source src={CuppaVideo} type="video/mp4" />
+            </video>
+
+            <div id="logo">
+                <div id="slogan">
+                    <h1 id="sloganText" className="flip-animate" target="_blank">
+                        Template<span data-hover="Flip">Flip</span>
+                    </h1>
+                    {/* {slogan.map((item) => {
+                        return
+                    })} */}
+                </div>
+                <img id="logoImg" src={Logo} alt="logo" />
+
+                <div id="contact">
+                    {toggle ? (
+                        <>
+                            <input
+                                id="inputBox"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                type="text"
+                                placeholder="enter your email"
+                            />
+                            <button id="sendBtn" onClick={emailBtnHandler}>
+                                Send
+                            </button>
+                        </>
+                    ) : (
+                        <>
+                            <input
+                                id="inputBox"
+                                value={feedback}
+                                onChange={(e) => setFeedback(e.target.value)}
+                                type="text"
+                                placeholder="Drop message"
+                            />
+                            <button id="sendBtn" onClick={feedbackBtnHandler}>
+                                Go
+                            </button>
+                        </>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,43 @@
+// firestore
+
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCJKt_rK1Y2nHkAxepvSeKeS1GtykND74c",
+//     authDomain: "cuppa-home-page.firebaseapp.com",
+//     databaseURL: "https://cuppa-home-page-default-rtdb.firebaseio.com",
+//     projectId: "cuppa-home-page",
+//     storageBucket: "cuppa-home-page.appspot.com",
+//     messagingSenderId: "957670161184",
+//     appId: "1:957670161184:web:7da37a9f388cc62761b545",
+//     measurementId: "G-4STCX9SC1K"
+//   };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+// export { db };
+
+// real-time
+
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+function startFirebase() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyCJKt_rK1Y2nHkAxepvSeKeS1GtykND74c",
+        authDomain: "cuppa-home-page.firebaseapp.com",
+        databaseURL: "https://cuppa-home-page-default-rtdb.firebaseio.com",
+        projectId: "cuppa-home-page",
+        storageBucket: "cuppa-home-page.appspot.com",
+        messagingSenderId: "957670161184",
+        appId: "1:957670161184:web:7da37a9f388cc62761b545",
+        measurementId: "G-4STCX9SC1K",
+    };
+
+    const app = initializeApp(firebaseConfig);
+    return getDatabase(app);
+}
+
+export default startFirebase;
