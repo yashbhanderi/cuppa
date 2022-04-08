ea9a9e0790c280efbf97b72ebf73306c5d485a53
Yash Bhanderi <yashbhanderi1818@gmail.com>
2022-04-08 21:03:02 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import { useEffect, useState } from "react";
 import CuppaVideo from "./video/cuppa.mp4";
-import Logo from "./Logo.png";
+import Logo from "./1 TRANSPARENT.png";
+import LogoBlack from "./Logo.png";
+import GooglePlay from "./images/google_play.png";
+import AppStore from "./images/app_store.png";
+import QRCode from "./images/qrcode.png";
 import "./style.css";
+// import 'font-awesome/css/font-awesome.min.css'
 
 import startFirebase from "./firebase";
-import { Timestamp } from "firebase/firestore";
 
 // firestore
 // import { db } from "./firebase";
 // import { collection, addDoc, Timestamp } from "firebase/firestore";
 
-// real-time
+// real-times
 import { ref, set } from "firebase/database";
 
 const App = () => {
-    // const slogan = ["dublin is ", "xyz is ", "abc is "];
+    const slogan = ["Dublin Start-up ", "Dublin love ", "I love "];
 
     const [feedback, setFeedback] = useState("");
     const [email, setEmail] = useState("");
     const [toggle, setToggle] = useState(false);
     const [db, setDb] = useState(null);
+    const [disable, setDisable] = useState(false);
+    const [showNotification, setShowNotification] = useState(false);
+    const [logoblack, setLogoblack] = useState(false);
 
     const feedbackBtnHandler = () => {
         if (feedback !== "") {
@@ -43,10 +50,10 @@ const App = () => {
         // }
 
         // real-time
-        
+
         const datetime = Date().toLocaleString();
 
-        set(ref(db, "feedback/"+datetime), {
+        set(ref(db, "feedback/" + datetime), {
             message: feedback,
             email: email,
             created: new Date().toLocaleString(),
@@ -56,11 +63,34 @@ const App = () => {
                 setToggle(false);
                 setFeedback("");
                 setEmail("");
+                setShowNotification(true);
+                setDisable(true);
             })
             .catch((err) => {
                 console.log("[Error] ", err);
             });
     };
+
+    const closeMessage = (e) => {
+        const elem = document.getElementById("notification");
+        setShowNotification(false);
+        elem.classList.add("right");
+    };
+
+    const playbackHandler = (e) => {
+        if (e.target.currentTime >= 20.75 && e.target.currentTime <= 24.75) {
+            setLogoblack(true);
+        } else {
+            setLogoblack(false);
+        }
+    };
+
+    useEffect(() => {
+        showNotification &&
+            setTimeout(() => {
+                closeMessage();
+            }, 3000);
+    }, [showNotification]);
 
     useEffect(() => {
         setDb(startFirebase());
@@ -68,50 +98,118 @@ const App = () => {
 
     return (
         <div id="home">
-            <video id="video" autoPlay loop muted>
+            <video onTimeUpdate={(e) => playbackHandler(e)} id="video" autoPlay loop muted>
                 <source src={CuppaVideo} type="video/mp4" />
             </video>
+            {/* <iframe
+                id="video"
+                src="https://www.youtube.com/embed/YBHQbu5rbdQ"
+                title="YouTube video player"
+                frameborder="0"
+                showinfo="0"
+                controls="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe> */}
 
             <div id="logo">
-                <div id="slogan">
-                    <h1 id="sloganText" className="flip-animate" target="_blank">
-                        Template<span data-hover="Flip">Flip</span>
-                    </h1>
-                    {/* {slogan.map((item) => {
-                        return
-                    })} */}
+                <div id="logoDiv">
+                    <div className="verticalflip">
+                        {/* I Am &nbsp; */}
+                        {slogan.map((item) => {
+                            return <span>{item}</span>;
+                        })}
+                    </div>
+                    {logoblack ? (
+                        <img id="logoImg" src={LogoBlack} alt="logo" />
+                    ) : (
+                        <img id="logoImg" src={Logo} alt="logo" />
+                    )}
                 </div>
-                <img id="logoImg" src={Logo} alt="logo" />
-
-                <div id="contact">
-                    {toggle ? (
-                        <>
+            </div>
+            <div id="contact">
+                <div
+                    id="notification"
+                    style={
+                        showNotification
+                            ? { marginLeft: "0%", transition: "all 1s" }
+                            : { marginLeft: "-100%", transition: "all 1s" }
+                    }
+                >
+                    <span>Message has been sent</span>
+                    <span onClick={closeMessage}>
+                        <i
+                            style={{
+                                color: "white",
+                                borderRadius: "4px",
+                                height: "20px",
+                                margin: "0 5px",
+                                width: "30px",
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center",
+                                backgroundColor: "red",
+                            }}
+                            className="fa-solid fa-xmark"></i>
+                    </span>
+                </div>
+                {toggle ? (
+                    <>
+                        <div
+                            style={{
+                                marginLeft: "10px",
+                                display: "flex",
+                                alignItems: "center",
+                                textAlign: "center",
+                            }}>
+                            <i id="msgIcon" className="fa-regular fa-envelope"></i>
                             <input
                                 id="inputBox"
+                                disabled={disable ? true : false}
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                                 type="text"
-                                placeholder="enter your email"
+                                placeholder="Email"
                             />
-                            <button id="sendBtn" onClick={emailBtnHandler}>
-                                Send
-                            </button>
-                        </>
-                    ) : (
-                        <>
+                        </div>
+                        <button id="sendBtn" onClick={emailBtnHandler}>
+                            Send
+                        </button>
+                    </>
+                ) : (
+                    <>
+                        <div
+                            style={{
+                                marginLeft: "10px",
+                                display: "flex",
+                                alignItems: "center",
+                                textAlign: "center",
+                            }}>
+                            <i id="msgIcon" className="fa-regular fa-message"></i>
                             <input
                                 id="inputBox"
+                                disabled={disable ? true : false}
                                 value={feedback}
                                 onChange={(e) => setFeedback(e.target.value)}
                                 type="text"
-                                placeholder="Drop message"
+                                placeholder="Drop Message"
                             />
-                            <button id="sendBtn" onClick={feedbackBtnHandler}>
-                                Go
-                            </button>
-                        </>
-                    )}
-                </div>
+                        </div>
+                        <button
+                            disabled={disable ? true : false}
+                            id="sendBtn"
+                            onClick={feedbackBtnHandler}>
+                            Go
+                        </button>
+                    </>
+                )}
+            </div>
+            <div id="iconLink">
+                <h1 id="iconText" style={{ padding: "5px" }}>
+                    Cuppa is a startup originated from Dublin. 
+                </h1>
+                <img style={{ height: "200px" }} src={QRCode} alt="" />
+                <img src={GooglePlay} alt="" />
+                <img src={AppStore} alt="" />
             </div>
         </div>
     );
